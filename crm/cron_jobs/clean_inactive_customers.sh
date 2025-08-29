1aad95e82f1df574d9fe9c8b5e58baa0d869f2cc
Menu Temesgen <menutemesgen@gmail.com>
2025-08-29 14:50:14 +0300

    Add script to clean inactive customers and schedule cron job

---
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+
+
+cd "$(dirname "$0")/../.."
+
+DELETED_COUNT=$(python3 manage.py shell -c "
+from django.utils import timezone
+from datetime import timedelta
+from crm.models import Customer
+
+one_year_ago = timezone.now() - timedelta(days=365)
+inactive_customers = Customer.objects.filter(
+    orders__isnull=True, created_at__lt=one_year_ago
+) | Customer.objects.exclude(orders__created_at__gte=one_year_ago)
+
+count = inactive_customers.count()
+inactive_customers.delete()
+print(count)
+")
+
+echo \"\$(date '+%Y-%m-%d %H:%M:%S') - Deleted \$DELETED_COUNT inactive customers\" >> /tmp/customer_cleanup_log.txt
